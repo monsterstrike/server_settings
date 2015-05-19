71c8e3b34d3210190a354c6556c36eff064f7080
Osamu MATSUMOTO <osamu.matsumoto@mixi.co.jp>
2015-05-19 12:42:52 +0900

    Fix cap role definition for ServerSettings::HostCollection

--- lib/server_settings/capistrano.rb
+++ lib/server_settings/capistrano.rb
@@ -8,7 +8,7 @@ module Capistrano
           def load_servers(pattern)
             ServerSettings.load_config_dir(pattern)
             ServerSettings.each_role do |role, hosts|
-              role role.to_sym, *hosts
+              role role.to_sym, *hosts.map(&:host)
             end
           end
         end
