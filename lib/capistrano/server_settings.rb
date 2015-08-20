bab24e8783fae927eef2b3390c3a2cdde3b30c09
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2015-08-20 20:14:06 +0900

    Remove splat operator (no need on capistrano3)

--- lib/capistrano/server_settings.rb
+++ lib/capistrano/server_settings.rb
@@ -3,6 +3,6 @@ require 'server_settings'
 def load_servers(pattern)
   ServerSettings.load_config_dir(pattern)
   ServerSettings.each_role do |role, hosts|
-    role role.to_sym, *hosts.map(&:host)
+    role role.to_sym, hosts.map(&:host)
   end
 end
