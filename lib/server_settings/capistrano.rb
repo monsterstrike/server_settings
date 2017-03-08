0105959103440e3fc2717fa7d7f99ad611d5e821
w1mvy <w1mvypine@gmail.com>
2017-03-08 11:19:46 +0900

    Correspond cap2 role option, `no_release`, `primary`, `active`

--- lib/server_settings/capistrano.rb
+++ lib/server_settings/capistrano.rb
@@ -8,8 +8,12 @@ module Capistrano
           def load_servers(pattern)
             ServerSettings.load_config_dir(pattern)
             ServerSettings.each_role do |role, hosts|
-              if hosts.class == ::ServerSettings::HostCollection && hosts.role_config["%no_release"]
-                role role.to_sym, *hosts.map(&:host), :no_release => true
+              if hosts.class == ::ServerSettings::HostCollection
+                role_properties              = {}
+                role_properties[:no_release] = true if hosts.properties["%no_release"]
+                role_properties[:primary]    = true if hosts.properties["%primary"]
+                role_properties[:active]     = true if hosts.properties["%active"]
+                role role.to_sym, *hosts.map(&:host), role_properties
               else
                 role role.to_sym, *hosts.map(&:host)
               end
